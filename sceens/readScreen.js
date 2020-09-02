b5b7fbe3de5f3ab1b4afbc72af388bc4f60b1f84
JiviteshJamuda <jivitesh2006@gmail.com>
2020-09-02 11:26:44 +0530

    list

--- sceens/readScreen.js
+++ sceens/readScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 import db from "../config"
+import { SearchBar, Icon } from 'react-native-elements';
 
 export default class ReadScreen extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            allStories : []
+            allStories : [],
+            search : "",
         }
     }
 
@@ -15,6 +17,8 @@ export default class ReadScreen extends React.Component {
         query.docs.map((doc)=>{
             this.setState({
                 allStories : [...this.state.allStories, doc.data()],
+                lastStory : null,
+                search :"",
             })
         });
         //console.log(this.state.allStories[0].Author);
@@ -34,13 +38,53 @@ export default class ReadScreen extends React.Component {
         )
       }
 
+      searchStories= async(text) =>{
+        var enteredText = text.split("")
+        var text = text.toUpperCase()
+            
+        if (enteredText[0].toUpperCase() ==='T'){
+          const story =  await db.collection("Stories").where('Title','==',text).get()
+          story.docs.map((doc)=>{
+            this.setState({
+              allStories:[...this.state.allTransactions,doc.data()],
+              lastStory: doc
+            })
+          })
+        }
+        else if(enteredText[0].toUpperCase() === 'A'){
+          const story = await db.collection('Stories').where('Author','==',text).get()
+          story.docs.map((doc)=>{
+            this.setState({
+              allStories:[...this.state.allTransactions,doc.data()],
+              lastStory: doc
+            })
+          })
+        }
+      }
+
     render(){
         return( 
                 <ScrollView>
+                    <SearchBar
+                        placeholder="Type here..."
+                        onChangeText={(search)=>{
+                          this.setState({ search : search })
+                        }}
+                        value={this.state.search}
+                        searchIcon={
+                            <Icon
+                                name="search"
+                                color="white"
+                                onPress={()=>{
+                                    this.searchStories(this.state.search);
+                                }}
+                            />
+                        }
+                    />
                     {
                         this.state.allStories.length === 0 ? 
                         (
-                            <View style={{flex:1}}>
+                            <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
                                 <Text style={{fontSize:40}}>Fetching the best stories...</Text>
                             </View>
                         )
