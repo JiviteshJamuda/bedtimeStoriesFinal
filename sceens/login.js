65afc78f6fdad93adbf75d79bec0e785dbe144bf
JiviteshJamuda <jivitesh2006@gmail.com>
2020-09-03 21:56:57 +0530

    final commit !!

--- sceens/login.js
+++ sceens/login.js
@@ -75,56 +75,6 @@ const styles = StyleSheet.create({
       justifyContent:'center',
       alignItems:'center'
   },
-  modalTitle :{
-      justifyContent:'center',
-      alignSelf:'center',
-      fontSize:30,
-      color:'#ff5722',
-      margin:50
-  },
-  modalContainer:{
-      flex:1,
-      borderRadius:20,
-      justifyContent:'center',
-      alignItems:'center',
-      backgroundColor:"#ffff",
-      marginRight:30,
-      marginLeft : 30,
-      marginTop:80,
-      marginBottom:80,
-  },
-  formTextInput:{
-      width:"75%",
-      height:35,
-      alignSelf:'center',
-      borderColor:'#ffab91',
-      borderRadius:10,
-      borderWidth:1,
-      marginTop:20,
-      padding:10
-  },
-  registerButton:{
-      width:200,
-      height:40,
-      alignItems:'center',
-      justifyContent:'center',
-      borderWidth:1,
-      borderRadius:10,
-      marginTop:30
-  },
-  registerButtonText:{
-      color:'#ff5722',
-      fontSize:15,
-      fontWeight:'bold'
-  },
-  cancelButton:{
-      width:200,
-      height:30,
-      justifyContent:'center',
-      alignItems:'center',
-      marginTop:5,
-  },
-     
   button:{
       width:300,
       height:50,
